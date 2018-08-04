5513f713a2df214b454f2e531782a0dea249f817
Sid <sidwlynn@gmail.com>
2018-08-04 09:37:40 +0100

    Second commit

--- src/main/MainThing.java
+++ src/main/MainThing.java
@@ -4,5 +4,6 @@ public class MainThing {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        WorkItOut.addTwoNumbers(3, 6);
     }
 }
---
+++ src/main/WorkItOut.java
@@ -0,0 +1,15 @@
+package main;
+
+public class WorkItOut {
+    int anInt;
+    int bnInt;
+
+    public WorkItOut(int anInt, int bnInt){
+        this.anInt=anInt;
+        this.bnInt=bnInt;
+    }
+
+    public static int addTwoNumbers(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+}
